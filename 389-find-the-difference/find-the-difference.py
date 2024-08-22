67f4aa07ecddef66ac996b0444aba197b985c1c6
muddymac <163457790+muddymac@users.noreply.github.com>
2024-08-22 22:48:06 +0530

    Time: 36 ms (69.60%) | Memory: 16.4 MB (91.62%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -1,3 +1,12 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        return chr(sum(map(ord, t)) - sum(map(ord, s)))
+        if len(s)>len(t):
+            for i in s:
+                if s.count(i)>t.count(i):
+                    return(i)
+                    break
+        if len(t)>len(s):
+            for i in t:
+                if t.count(i)>s.count(i):
+                    return(i)
+                    break
