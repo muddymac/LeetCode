a9f0baa7d0267e09964f7c21944bf321e27138c3
muddymac <163457790+muddymac@users.noreply.github.com>
2024-08-23 10:20:06 +0530

    Time: 4074 ms (5.01%) | Memory: 19 MB (91.50%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         mapper = {}
+        # [2, 2, 1]
+        for num in nums:
+            if nums.count(num) == 1:
+                return num
+        '''
         for num in nums:
             if num in mapper:
                 mapper[num] += 1
@@ -9,4 +14,6 @@ class Solution:
         for _map in mapper:
             if mapper.get(_map) == 1:
                 return _map
+        '''
+        
         
